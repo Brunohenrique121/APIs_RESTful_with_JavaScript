0b136b9a128ebd77bc2db5772911231d87fccb8d
ALUNO SENAI PETROLINA <aluno.senaipetrolina@SISTEMAFIEPE.LOCAL>
2026-02-10 10:29:10 -0300

    Atualizando arquivos do projeto

--- restaurante-api-demo/backend/src/services/database.js
+++ restaurante-api-demo/backend/src/services/database.js
@@ -1,16 +1,18 @@
 const mysql = require("mysql2/promise");
 require("dotenv").config();
 
-console.log(process.env.DB_USER, process.env.DB_HOST, process.env.DB_PASSWORD);
+//console.log(process.env.DB_USER, process.env.DB_HOST, process.env.DB_PASSWORD);
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: Number(process.env.DB_PORT),
-    waitForConnections: true,
-    connectionLimit: 10
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT,
+    ssl: {
+        minVersion: 'TLSv1.2',
+        rejectUnauthoried: true
+    },
 });
 
 ;
